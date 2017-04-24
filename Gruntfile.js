48b60fbefbf045051e3cc8e98052b40bdc9e03f7
Ania M <abromba3@gmail.com>
2017-04-24 11:20:26 +0200

    zmiany wcięć

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,42 +3,42 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     sass: {
-        options: {
-          sourceMap: true
-        },
-        dist: {
-          files: {
-            'css/main.css': 'sass/main.sass'
-          }
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
         }
       }
-    imagemin: {
-        dynamic: {
-           files: [{
-               expand: true,
-               cwd: 'images/',
-               src: ['https://images.pexels.com/photos/2988/pattern-triangles.jpg?h=350&auto=compress&cs=tinysrgb.{png,jpg,gif}'],
-               dest: 'images/build/'
-           }]
-        }
     }
-      watch: {
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['https://images.pexels.com/photos/2988/pattern-triangles.jpg?h=350&auto=compress&cs=tinysrgb.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    }
+    watch: {
       scripts: {
-          files: ['sass/main.sass'],
-          tasks: ['sass'],
-          options: {
-              spawn: false,
-          },
+        files: ['sass/main.sass'],
+        tasks: ['sass'],
+        options: {
+           spawn: false,
+        },
       } 
     }
     browserSync: {
       bsFiles: {
-          src : 'css/main.css'
+        src : 'css/main.css'
       },
       options: {
-          server: {
-              baseDir: "file:///C:/Users/b/Documents/Ania/programowanie/zadania%20domowe/GIT/index.html"
-          }
+        server: {
+           baseDir: "./"
+        }
       }
     }
   });
@@ -49,5 +49,4 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
   grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
-
 };
